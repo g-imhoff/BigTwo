ddb85ecb6a0656002acd305714e3b0753df72304
BARBUTOV FILIP <filip.barbutov@etu.unistra.fr>
2025-03-19 15:15:07 +0100

    buttons changed on log/create choose, and added back buttons on all pages

--- godot_project/GameStarter/connection_page.gd
+++ godot_project/GameStarter/connection_page.gd
@@ -6,3 +6,7 @@ func _on_login_button_pressed() -> void:
 
 func _on_register_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://GameStarter/CreatePage.tscn")
+
+
+func _on_backicon_connection_page_pressed() -> void:
+	get_tree().change_scene_to_file("res://GameStarter/HomePage.tscn")
