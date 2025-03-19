5041254859236417bdd0b6e0508582d3679084be
Seifeldin Kamel <seifeldin.kamel@etu.unistra.fr>
2025-03-19 16:18:04 +0100

    new page for profile

--- godot_project/GameStarter/rules_page.gd
+++ godot_project/GameStarter/rules_page.gd
@@ -7,3 +7,7 @@ func _on_lets_play_pressed() -> void:
 
 func _on_backicon_rules_pressed() -> void:
 	get_tree().change_scene_to_file("res://GameStarter/HomePage.tscn")
+
+
+func _on_background_draw() -> void:
+	pass # Replace with function body.
