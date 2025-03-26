5dc8931cc787116324305ea6bdc94a427aa5cee4
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2025-03-26 13:12:25 +0000

    ready to test

--- godot_project/GameOnline/scripts/client_game.gd
+++ godot_project/GameOnline/scripts/client_game.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 var socket = WebSocketPeer.new()
 
-const CARD_SCENE_PATH= "res://Game/scenes/cartes.tscn"
-var center_screen_x
+@onready var hand = $PlayerHand
 
 func _on_tree_exited() -> void:
 	socket.close()
@@ -41,7 +40,7 @@ func _data_received_handler(data):
 			# Connected to the server game
 			pass
 		1: 
-			_card_hand_init(data["card_hand"], data["first_player"])
+			hand._card_hand_init(data["card_hand"], data["first_player"])
 
 func _server_handshake():
 	var content = JSON.stringify({
@@ -50,18 +49,3 @@ func _server_handshake():
 	})
 	
 	socket.send_text(content)
-
-func _card_hand_init(list_card, bool_first_player):
-	center_screen_x=get_viewport().size.x/2
-	var card_scene=preload(CARD_SCENE_PATH)
-	for card in list_card: 
-		var new_card=card_scene.instantiate()
-		var sprite=new_card.get_node("Sprite")
-		
-		var new_texture=load(card)
-		var card_info = Global.get_card_info_from_texture(card)
-		new_card.value = card_info[1]
-		new_card.form = card_info[0]
-		new_card.name="Card"
-		sprite.texture=new_texture
-		$"../card_manager".add_child(new_card)
---
+++ godot_project/GameOnline/scripts/player_hand.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+const CARD_SCENE_PATH= "res://Game/scenes/cartes.tscn"
+var center_screen_x
+var player_hand=[]
+const HAND_Y_POSITION=870
+const CARD_WIDTH=80
+
+func _card_hand_init(list_card, bool_first_player):
+	center_screen_x=get_viewport().size.x/2
+	var card_scene=preload(CARD_SCENE_PATH)
+	for card in list_card: 
+		var new_card=card_scene.instantiate()
+		var sprite=new_card.get_node("Sprite")
+		
+		var new_texture=load(card)
+		var card_info = Global.get_card_info_from_texture(card)
+		new_card.value = card_info[1]
+		new_card.form = card_info[0]
+		new_card.name="Card"
+		sprite.texture=new_texture
+		$"../card_manager".add_child(new_card)
+		add_card_to_hand(new_card)
+
+func add_card_to_hand(card):
+	if card not in player_hand:
+		player_hand.append(card)
+		update_hand_position()
+	else:
+		animate_card_to_position(card, card.hand_position)
+
+func update_hand_position():
+	for i in range (player_hand.size()):
+		var new_position =Vector2(calculate_card_position(i),HAND_Y_POSITION)
+		var card=player_hand[i]
+		card.hand_position=new_position
+		animate_card_to_position(card,new_position)
+
+
+func calculate_card_position(index):
+	var total_with=(player_hand.size()-1)*CARD_WIDTH 
+	var x_offset=center_screen_x + index * CARD_WIDTH - total_with/2
+	return x_offset
+	
+func animate_card_to_position(card,new_position):
+	var tween = get_tree().create_tween()
+	tween.tween_property(card,"position",new_position,0.1 )
+
+func remove_card_from_hand(card):
+	if card in player_hand:
+		player_hand.erase(card)
