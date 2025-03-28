d8b5575c106115dce81a977a72bb8b3e18475ac6
KAMEL SEIFELDIN <seifeldin.kamel@etu.unistra.fr>
2025-03-28 12:49:57 +0100

    changment dans le setting page

--- godot_project/GameStarter/setting.gd
+++ godot_project/GameStarter/setting.gd
@@ -4,5 +4,5 @@ func make_visible(ismenu) -> void:
 	self.set_visible(true) 
 
 
-func _on_texture_button_pressed() -> void:
-		self.set_visible(false)
+func _on_button_pressed() -> void:
+	get_tree().quit()
