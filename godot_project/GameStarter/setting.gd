9f7f5e62baf17fbd1436d5586551638b98d216c5
BARBUTOV FILIP <filip.barbutov@etu.unistra.fr>
2025-04-02 15:27:46 +0200

    sound settings effects on/off

--- godot_project/GameStarter/setting.gd
+++ godot_project/GameStarter/setting.gd
@@ -23,3 +23,18 @@ func _on_h_slider_value_changed(value: float) -> void:
 	Config.volume_value = value  # Modifie la valeur du volume dans le script autoload
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("BGMusic"), value)
 	print(value)
+
+
+func _on_check_button_music_toggled(toggled_on: bool) -> void:
+	if toggled_on==false:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("BGMusic"),true)
+	else:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("BGMusic"),false)
+		
+
+
+func _on_check_button_button_sound_toggled(toggled_on: bool) -> void:
+	if toggled_on==false:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Sfx"),true)
+	else:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Sfx"),false)
