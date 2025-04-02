b34e9ba4688c16e092dec9b94930face43057c74
BARBUTOV FILIP <filip.barbutov@etu.unistra.fr>
2025-04-02 14:05:31 +0200

    taking out sound slider homepage and making the one in settings work

--- godot_project/GameStarter/setting.gd
+++ godot_project/GameStarter/setting.gd
@@ -11,3 +11,7 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://GameStarter/HomePage.tscn")
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("BGMusic"), value)
