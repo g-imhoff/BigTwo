4bfa8ca81b0c252a2ba421c3a8e3cbfc67c0f814
BARBUTOV FILIP <filip.barbutov@etu.unistra.fr>
2025-04-02 13:59:37 +0200

    fix settings button

--- godot_project/GameStarter/setting.gd
+++ godot_project/GameStarter/setting.gd
@@ -6,3 +6,8 @@ func make_visible(ismenu) -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://GameStarter/HomePage.tscn")
