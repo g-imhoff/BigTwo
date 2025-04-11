7274f3ef9b16de7b347ca5c271002eb6780d5112
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2025-04-11 09:24:58 +0100

    combi debug

--- backend/game_online/python_projet.py
+++ backend/game_online/python_projet.py
@@ -38,31 +38,22 @@ def check_higher_than_previous(previous_combi, new_combi):
             return False, "Your combinaison is not higher than the previous one"
 
 def check_card_clicked(card_clicked):
-    check=None
-    combi=None
+    combi=Combinaison(None, None, None)
+    card_clicked.sort(key=lambda card: card.value)
+
     if len(card_clicked) == 1:
-        check=1
-        combi=1
-        combi_value=card_clicked[0].value
-        combi_form=card_clicked[0].form 
+        combi.combi=1
+        combi.combi_value=card_clicked[0].value
+        combi.combi_form=card_clicked[0].form 
     elif len(card_clicked) == 2 and card_clicked[0].value==card_clicked[1].value:
-        check=2
-        combi=2
-        combi_value=card_clicked[0].value
-        lst_tmp=[]
-        lst_tmp = card_clicked.copy() 
-        lst_tmp.sort(key=lambda card: card.form)
-        combi_form=card_clicked[0].form
+        combi.combi=2
+        combi.combi_value=card_clicked[0].value
+        combi.combi_form=card_clicked[0].form
     elif len(card_clicked) == 3 and card_clicked[0].value == card_clicked[1].value and card_clicked[1].value == card_clicked[2].value:
-        check=3
-        combi=3
-        combi_value=card_clicked[0].value
-        lst_tmp=[]
-        lst_tmp = card_clicked.copy() 
-        lst_tmp.sort(key=lambda card: card.form)
-        combi_form=card_clicked[0].form
+        combi.combi=3
+        combi.combi_value=card_clicked[0].value
+        combi.combi_form=card_clicked[0].form
     elif len(card_clicked)==5:
-        card_clicked.sort(key=lambda card: card.value)
         val=0
         signe=0
         suite=0
@@ -84,28 +75,24 @@ def check_card_clicked(card_clicked):
             if card_clicked[i+1].value== card_clicked[i].value+1:
                 suite+=1
         if val == 3:#check Four of a Kind
-            check=7
-            combi=7
+            combi.combi=7
             val_tmp=card_clicked[0]
             if card_clicked[0]!=card_clicked[1]:
                 val_tmp=card_clicked[1]
-            combi_value=val_tmp.value
-            combi_form=val_tmp.form
+            combi.combi_value=val_tmp.value
+            combi.combi_form=val_tmp.form
         elif suite==4 and signe ==4:#check pour Straight Flush
-            check =8 
-            combi=8
-            combi_value=card_clicked[4].value
-            combi_form=card_clicked[4].form
+            combi.combi=8
+            combi.combi_value=card_clicked[4].value
+            combi.combi_form=card_clicked[4].form
         elif signe==4:#check Flush
-            check= 5
-            combi=5
-            combi_value=card_clicked[4].value
-            combi_form=card_clicked[4].form
+            combi.combi=5
+            combi.combi_value=card_clicked[4].value
+            combi.combi_form=card_clicked[4].form
         elif suite==4:#check pour straight
-            check =4
-            combi=4
-            combi_value=card_clicked[4].value
-            combi_form=card_clicked[4].form
+            combi.combi=4
+            combi.combi_value=card_clicked[4].value
+            combi.combi_form=card_clicked[4].form
         elif val == 1 or val == 2:  # check pour full house
             tab_val = card_clicked.copy()
             for i in range(len(tab_check_brelan)):
@@ -113,20 +100,13 @@ def check_card_clicked(card_clicked):
             if val == 2:
                 brelan=tab_check_brelan.duplicate()
                 if tab_val[0].value == tab_val[1].value:
-                    check = 6
-                    combi=6
-                    combi_value=brelan[0].value
-                    combi_form=brelan[0].form		
+                    combi.combi=6
+                    combi.combi_value=brelan[0].value
+                    combi.combi_form=brelan[0].form		
             elif val == 1:
                 brelan=tab_val.copy()
                 if len(tab_val) == 3 and tab_val[0].value == tab_val[1].value:  # Assure que seulement 2 cartes restent
-                    combi=6
-                    check = 6
-                    combi_value=tab_val[0].value
-                    combi_form=tab_val[0].form
-    return check
-
-
-
-
-    
+                    combi.combi=6
+                    combi.combi_value=tab_val[0].value
+                    combi.combi_form=tab_val[0].form
+    return combi 
