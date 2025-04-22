b3aca5722d28060a636f1acce673a937e66a88c6
KAMEL SEIFELDIN <seifeldin.kamel@etu.unistra.fr>
2025-04-22 22:01:59 +0200

    added two pop to the lobby page on for creating and the other on for joining

--- godot_project/GameStarter/lobby.gd
+++ godot_project/GameStarter/lobby.gd
@@ -3,3 +3,11 @@ extends Node2D
 
 func _on_back_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://GameStarter/ChooseModePage.tscn")
+
+
+func _on_create_room_btn_pressed() -> void:
+	$Createroompop.visible = not $Createroompop.visible
+
+
+func _on_join_room_btn_pressed() -> void:
+	$Joinroompop.visible = not $Joinroompop.visible
