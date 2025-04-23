f3cf2d702dfe88fcc7d9c01b8f332b3a9c6cd559
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2025-04-23 13:04:14 +0100

    fix possition

--- godot_project/GameOnline/scripts/player_hand.gd
+++ godot_project/GameOnline/scripts/player_hand.gd
@@ -55,7 +55,7 @@ func update_hand_position():
 func calculate_card_position(index):
 	var total_width=(player_hand.size()-1) * CARD_WIDTH
 	var center_screen_x = DisplayServer.window_get_size().x/2
-	var x_offset=center_screen_x + (index * CARD_WIDTH) + total_width/2
+	var x_offset=center_screen_x + (index * CARD_WIDTH) - total_width/2
 	return x_offset
 	
 func animate_card_to_position(card,new_position):
