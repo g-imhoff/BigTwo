e21fe335a36808d86332d4aabeecf6ea76e21fe6
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2025-04-29 23:27:14 +0100

    add http api request logic to client

--- godot_project/GameStarter/login_page.gd
+++ godot_project/GameStarter/login_page.gd
@@ -3,7 +3,6 @@ extends Node2D
 @onready var profile_name_email_line_edit = $Email/LabelEmail
 @onready var password_line_edit = $Password/LabelPassword
 @onready var rememberme_checkbox = $RememberMe
-var socket = WebSocketPeer.new()
 
 var Hash = load("res://hashage.gd")
 
@@ -13,6 +12,10 @@ func _on_oauth_google_input_event(_viewport: Node, event: InputEvent, _shape_idx
 
 func _on_login_pressed() -> void:
 	# Assuming these are the variable names for the LineEdit nodes
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.request_completed.connect(_http_request_completed)
+	
 	var profile_name_email = profile_name_email_line_edit.text
 	var password = password_line_edit.text
 	var rememberme = rememberme_checkbox.is_pressed()
@@ -22,59 +25,36 @@ func _on_login_pressed() -> void:
 		password_hash = Hash.hash_password(password)
 	
 	var content = JSON.stringify({
-	"function": "login",
-	"data": {
-		"profile_name_email": profile_name_email,
+		"username_email": profile_name_email,
 		"password": password_hash
-	}})
+	})
 		
-	socket.send_text(content)
+	var error = http_request.request(Global.api_url + "/auth/login", [], HTTPClient.METHOD_POST, content)
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
 
 
 func _on_create_account_pressed() -> void: 
 	get_tree().change_scene_to_file("res://GameStarter/CreatePage.tscn")
 
-
 func _ready() -> void:
-	var clientCAS = load("res://cert.crt")
-	var err = socket.connect_to_url(Global.websocket_url, TLSOptions.client_unsafe(clientCAS))
-	if err != OK:
-		print("Unable to connect")
-		set_process(false)
-		
 	UISounds.install_sounds(self)
 
-func _process(_delta):
-	socket.poll()
-	var state = socket.get_ready_state()
-	if state == WebSocketPeer.STATE_OPEN: 
-		while socket.get_available_packet_count():
-			_data_received_handler(JSON.parse_string(socket.get_packet().get_string_from_utf8()))
-	elif state == WebSocketPeer.STATE_CLOSING:
-		# Keep polling to achieve proper close.
-		pass
-	elif state == WebSocketPeer.STATE_CLOSED:
-		var code = socket.get_close_code()
-		var reason = socket.get_close_reason()
-		print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
-		set_process(false) # Stop processing.
-		get_tree().change_scene_to_file("res://GameStarter/ConnectionPage.tscn")
+func _http_request_completed(result, response_code, headers, body): 
+	var json = JSON.new()
+	json.parse(body.get_string_from_utf8())
+	var response = json.get_data()
 
-func _data_received_handler(data):
-	if (data["code"] == 0):
+	if (response["code"] == 0):
 		# Needs to setup a token of connection
-		Global.username = data["username"]
+		Global.username = response["username"]
 		get_tree().change_scene_to_file("res://GameStarter/ChooseModePage.tscn")
 	else :
-		Notification.show_side(data["message"])
-
-func _on_tree_exited() -> void:
-	socket.close()
+		Notification.show_side(response["message"])
 
 
 func _on_back_button_login_pressed() -> void:
 	get_tree().change_scene_to_file("res://GameStarter/ConnectionPage.tscn")
-
 
 func _on_texture_button_pressed() -> void:
 		print("OAuthGoogleClicked")
