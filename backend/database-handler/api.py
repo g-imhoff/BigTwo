d9fadb785103a70994f6b5876364b7f2c20ce517
vm8.0 <none>
2025-04-29 22:26:02 +0000

    turned from a websocket to an api for the login and register, added also some hotfix

--- backend/database-handler/api.py
+++ backend/database-handler/api.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+import uvicorn
 from bdd_script import create_account, login_account
 
 app = FastAPI()
@@ -18,11 +19,10 @@ login_account_error = [
 ]
 
 
-@app.get("/auth/login")
-def login(
-    username_email: str,
-    password: str
-):
+@app.post("/auth/login")
+def login(data: dict = Body(...)):
+    username_email = data.get("username_email")
+    password = data.get("password")
 
     print("Trying a login from", username_email, password)
     result, username = login_account(username_email, password)
@@ -32,15 +32,18 @@ def login(
     return {"code": result, "message": login_account_error[result], "username": username}
 
 
-@app.get("/auth/register")
-def register(
-    username: str,
-    email: str,
-    password: str
-):
+@app.post("/auth/register")
+def register(data: dict = Body(...)):
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+
     print("Trying a register from", username, email, password)
     result = create_account(username, email, password)
     print(create_account_error[result],
           username, email, password)
 
     return {"code": result, "message": create_account_error[result]}
+
+if __name__ == "main":
+    uvicord.run(app, host="0.0.0.0", port=10005)
